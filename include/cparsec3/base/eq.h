02f568ecf4b38160ce6ba4f267d886f7a3bbaacf
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-10-27 16:03:35 +0900

    refactored Eq(T) and added Eq(None)

--- include/cparsec3/base/eq.h
+++ include/cparsec3/base/eq.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "module.h"
+#include "typeset.h"
 
 #define Eq(T) CONCAT(Eq, T)
 #define declare_Eq(T)                                                    \
@@ -15,17 +16,4 @@
   C_API_END                                                              \
   END_OF_STATEMENTS
 
-declare_Eq(char);
-declare_Eq(int);
-
-declare_Eq(int8_t);
-declare_Eq(int16_t);
-declare_Eq(int32_t);
-declare_Eq(int64_t);
-
-declare_Eq(uint8_t);
-declare_Eq(uint16_t);
-declare_Eq(uint32_t);
-declare_Eq(uint64_t);
-
-declare_Eq(String);
+FOREACH(declare_Eq, TYPESET(ALL));
