ff8b08c5eb2de636ca2485c7560a9c78c4646b1b
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-11-08 23:35:42 +0900

    added BIND, BIND_FOR, BIND_APPLY, BIND_SEP_BY macros

--- include/cparsec3/base/metac2.h
+++ include/cparsec3/base/metac2.h
@@ -248,6 +248,29 @@
 // FOR_EACH(F, (a,b), (c,d))
 // -> F(a,b) F(c,d)
 
+#define BIND(a, ...) BIND_SEP_BY(COMMA, , a, __VA_ARGS__)
+#define BIND_FOR(f, a, ...) BIND_SEP_BY(SEMICOLON, f, a, __VA_ARGS__)
+#define BIND_APPLY(f, a, ...) BIND_SEP_BY(COMMA, f, a, __VA_ARGS__)
+#define BIND_SEP_BY(sep, f, a, ...)                                      \
+  BIND_EVAL(BIND1(sep, f, a, __VA_ARGS__, ))
+#define BIND_INDIRECT() BIND1
+#define BIND1(sep, f, a, x, ...)                                         \
+  f(DISCLOSE(a), x) IF(IS_NIL(__VA_ARGS__))(                             \
+      , DEFER2(sep)() DEFER2(BIND_INDIRECT)()(sep, f, a, __VA_ARGS__))
+#define BIND_EVAL1(...) __VA_ARGS__
+#define BIND_EVAL2(...) BIND_EVAL1(BIND_EVAL1(__VA_ARGS__))
+#define BIND_EVAL3(...) BIND_EVAL2(BIND_EVAL2(__VA_ARGS__))
+#define BIND_EVAL4(...) BIND_EVAL3(BIND_EVAL3(__VA_ARGS__))
+#define BIND_EVAL5(...) BIND_EVAL4(BIND_EVAL4(__VA_ARGS__))
+#define BIND_EVAL6(...) BIND_EVAL5(BIND_EVAL5(__VA_ARGS__))
+#define BIND_EVAL7(...) BIND_EVAL6(BIND_EVAL6(__VA_ARGS__))
+#define BIND_EVAL8(...) BIND_EVAL7(BIND_EVAL7(__VA_ARGS__))
+#define BIND_EVAL(...) BIND_EVAL8(__VA_ARGS__)
+// BIND(a, b, c, d)
+// -> (a, b), (a, c), (a, d)
+// BIND((a,b), c, d)
+// -> (a,b,c), (a,b,d)
+
 #define GENERIC(expr, f, g, ...)                                         \
   _Generic((expr), GENERIC_SELECTORS(f, g, __VA_ARGS__))
 #define GENERIC_SELECTORS(f, g, ...)                                     \
