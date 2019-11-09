e41874a5dee4b1daae3c85e6ab364f299b2f54ef
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-11-09 13:01:42 +0900

    added `ArrayT(T) trait(Array(T))`

--- include/cparsec3/base/array.h
+++ include/cparsec3/base/array.h
@@ -4,8 +4,60 @@
 #include "common.h"
 
 #define Array(T) TYPE_NAME(Array, T)
+#define ArrayT(T) TYPE_NAME(ArrayT, T)
+// -----------------------------------------------------------------------
 #define typedef_Array(T)                                                 \
+  C_API_BEGIN                                                            \
   typedef struct {                                                       \
     size_t length;                                                       \
     T* data;                                                             \
-  } Array(T)
+  } Array(T);                                                            \
+  C_API_END                                                              \
+  END_OF_STATEMENTS
+
+// -----------------------------------------------------------------------
+#define trait_Array(T)                                                   \
+  C_API_BEGIN                                                            \
+  typedef struct {                                                       \
+    Array(T) (*create)(size_t n);                                        \
+    void (*free)(Array(T) a);                                            \
+    T* (*begin)(Array(T) a);                                             \
+    T* (*end)(Array(T) a);                                               \
+    size_t (*length)(Array(T) a);                                        \
+    bool (*empty)(Array(T) a);                                           \
+  } ArrayT(T);                                                           \
+  ArrayT(T) Trait(Array(T));                                             \
+  C_API_END                                                              \
+  END_OF_STATEMENTS
+
+// -----------------------------------------------------------------------
+#define impl_Array(T)                                                    \
+  C_API_BEGIN                                                            \
+  static Array(T) FUNC_NAME(create, Array(T))(size_t n) {                \
+    return (Array(T)){.length = n, .data = trait(Mem(T)).create(n)};     \
+  }                                                                      \
+  static void FUNC_NAME(free, Array(T))(Array(T) a) {                    \
+    trait(Mem(T)).free(a.data);                                          \
+  }                                                                      \
+  static T* FUNC_NAME(begin, Array(T))(Array(T) a) {                     \
+    return a.data;                                                       \
+  }                                                                      \
+  static T* FUNC_NAME(end, Array(T))(Array(T) a) {                       \
+    return a.data + a.length;                                            \
+  }                                                                      \
+  static size_t FUNC_NAME(length, Array(T))(Array(T) a) {                \
+    return a.length;                                                     \
+  }                                                                      \
+  static bool FUNC_NAME(empty, Array(T))(Array(T) a) {                   \
+    return !a.length;                                                    \
+  }                                                                      \
+  ArrayT(T) Trait(Array(T)) {                                            \
+    return (ArrayT(T)){.create = FUNC_NAME(create, Array(T)),            \
+                       .free = FUNC_NAME(free, Array(T)),                \
+                       .begin = FUNC_NAME(begin, Array(T)),              \
+                       .end = FUNC_NAME(end, Array(T)),                  \
+                       .length = FUNC_NAME(length, Array(T)),            \
+                       .empty = FUNC_NAME(empty, Array(T))};             \
+  }                                                                      \
+  C_API_END                                                              \
+  END_OF_STATEMENTS
