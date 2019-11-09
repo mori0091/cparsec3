03400524c25b70fd639ba2eb9aa2dd53e6bdbde3
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-11-09 11:37:43 +0900

    moved impl_Mem(T) macro from mem.c to mem.h

--- include/cparsec3/base/mem.h
+++ include/cparsec3/base/mem.h
@@ -18,4 +18,25 @@
   C_API_END                                                              \
   END_OF_STATEMENTS
 
+// -----------------------------------------------------------------------
+#define impl_Mem(T)                                                      \
+  C_API_BEGIN                                                            \
+  static T* FUNC_NAME(create, T)(size_t n) {                             \
+    return (T*)malloc(n * sizeof(T));                                    \
+  }                                                                      \
+  static T* FUNC_NAME(recreate, T)(T * ptr, size_t n) {                  \
+    return (T*)realloc(ptr, n * sizeof(T));                              \
+  }                                                                      \
+  static void FUNC_NAME(free, T)(T * p) {                                \
+    free((void*)p);                                                      \
+  }                                                                      \
+  Mem(T) Trait(Mem(T)) {                                                 \
+    return (Mem(T)){.create = FUNC_NAME(create, T),                      \
+                    .recreate = FUNC_NAME(recreate, T),                  \
+                    .free = FUNC_NAME(free, T)};                         \
+  }                                                                      \
+  C_API_END                                                              \
+  END_OF_STATEMENTS
+
+// -----------------------------------------------------------------------
 FOREACH(trait_Mem, TYPESET(ALL));
