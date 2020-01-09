c5fbcd356f1d805d6d41c687955b91aa1ebac274
mori0091 <mori-d@qc4.so-net.ne.jp>
2020-01-10 01:12:56 +0900

    added `bool` and `size_t` as a member of `TYPESET(ALL)`

--- include/cparsec3/base/typeset.h
+++ include/cparsec3/base/typeset.h
@@ -10,6 +10,6 @@
 #define TYPESET_STD_INT                                                  \
   int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t
 
-#define TYPESET_PRIMITIVE None, char, String, TYPESET_STD_INT
+#define TYPESET_PRIMITIVE None, bool, char, String, TYPESET_STD_INT
 
-#define TYPESET_ALL int, TYPESET_PRIMITIVE
+#define TYPESET_ALL int, size_t, TYPESET_PRIMITIVE
