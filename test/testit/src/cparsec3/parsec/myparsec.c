05ba05406426b1a5d15f60f0a1ab548f697c860f
mori0091 <mori-d@qc4.so-net.ne.jp>
2020-03-25 23:49:39 +0900

    added testcases of ParsecChar ; parsers regarding "ctype"

---
+++ test/testit/src/cparsec3/parsec/myparsec.c
@@ -0,0 +1,5 @@
+/* -*- coding: utf-8-unix -*- */
+
+/* generate parsec code if defined */
+#define CPARSEC_CONFIG_IMPLEMENT
+#include "myparsec.h"
---
+++ test/testit/src/cparsec3/parsec/myparsec.h
@@ -0,0 +1,11 @@
+/* -*- coding: utf-8-unix -*- */
+
+#define CPARSEC_CONFIG_DATA_SOURCE String
+#define CPARSEC_CONFIG_POSITIONER
+
+#include <cparsec3/easy_parsec/parser/char.h>
+#include <cparsec3/easy_parsec/parser/choice.h>
+#include <cparsec3/easy_parsec/parser/combinator.h>
+#include <cparsec3/easy_parsec/parser/failure.h>
+#include <cparsec3/easy_parsec/parser/repeat.h>
+#include <cparsec3/easy_parsec/parser/token.h>
